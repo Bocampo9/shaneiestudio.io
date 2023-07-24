c96c567c5650578c2f1584450be8dd69a6c5c975
Bocampo <ocampobraian@gmail.com>
2023-07-24 00:35:28 -0300

    final

--- shanei.js
+++ shanei.js
@@ -58,33 +58,8 @@ $(document).ready(function () {
   });
 });
 
-
-/* mapa */
-
-// Initialize and add the map
-let map;
-
-async function initMap() {
-  // The location of Uluru
-  const position = { lat: -34.7362, lng: -58.4473};
-  // Request needed libraries.
-  //@ts-ignore
-  const { Map } = await google.maps.importLibrary("maps");
-  const { AdvancedMarkerView } = await google.maps.importLibrary("marker");
-
-  // The map, centered at Uluru
-  map = new Map(document.getElementById("map"), {
-    zoom: 15,
-    center: position,
-    mapId: "DEMO_MAP_ID",
-  });
-
-  // The marker, positioned at Uluru
-  const marker = new AdvancedMarkerView({
-    map: map,
-    position: position,
-    title: "Uluru",
-  });
-}
-
-initMap();
+$(document).ready(function(){
+  $('.menu-icon').on('click',function(){
+      $('nav').slideToggle();
+  })
+})
