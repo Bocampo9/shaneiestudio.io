afa088ba5db30d73e40d30e00365ef0a73a38552
Bocampo <ocampobraian@gmail.com>
2023-07-25 20:55:37 -0300

    shanei

--- shanei.js
+++ shanei.js
@@ -1,47 +1,33 @@
-
-/* funcion popup */
-
 function openModal(category, event) {
-  event.preventDefault(); // Evitar el desplazamiento hacia el inicio de la página
+  event.preventDefault();
   var modalId = "modal-" + category;
   var modal = document.getElementById(modalId);
   modal.style.display = "block";
 }
-
 function closeModal(modalId) {
   var modal = document.getElementById(modalId);
   modal.style.display = "none";
 }
-
-
-/* funcion para que muestre las imagenes de los botones */
-
 function mostrarImagenes(id) {
   var imagenes = document.getElementsByClassName('grid-gallery__image');
-
-  // Mostrar todas las imágenes
   for (var i = 0; i < imagenes.length; i++) {
     imagenes[i].style.display = 'block';
   }
-
-  // Si se selecciona un ID específico, ocultar las imágenes que no coinciden
   if (id !== 'todos') {
     var imagenesOcultar = document.querySelectorAll('[id^="img-"]:not([id^="img-' + id + '"])');
     for (var j = 0; j < imagenesOcultar.length; j++) {
       imagenesOcultar[j].style.display = 'none';
     }
   }
-
-  // Resaltar el botón seleccionado
   var botones = document.getElementsByClassName('btn');
   for (var k = 0; k < botones.length; k++) {
     botones[k].classList.remove('btn-selected');
   }
   var botonSeleccionado = document.getElementById('btn-' + id);
-  botonSeleccionado.classList.add('btn-selected');
+  if (botonSeleccionado) {
+    botonSeleccionado.classList.add('btn-selected');
+  }
 }
-
-
 $(document).ready(function () {
   $(".accordion-titel").click(function () {
     $(this).parent(".accordion-item").find(".accordion-contant").slideToggle();
@@ -57,9 +43,10 @@ $(document).ready(function () {
       .slideUp();
   });
 });
-
 $(document).ready(function(){
   $('.menu-icon').on('click',function(){
       $('nav').slideToggle();
   })
 })
+
+
